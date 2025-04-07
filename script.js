419532427ef34e884bb923b0251c8a0b828d6cbb
bonnie <130217662+realbxnnie@users.noreply.github.com>
2025-04-07 16:14:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Generated with CSharpScript
+
+function buy_carrot() {
+    window.open("https://www.donationalerts.com/r/thisisbonnie");
+}
+
+function github() {
+    window.open("https://github.com/realbxnnie");
+}
+
+function twitch() {
+    window.open("https://www.twitch.tv/b0nn1ee_");
+}
